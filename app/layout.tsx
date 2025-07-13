364ff7ddad8a4e1c623e3cf4f38a3c5a3c9e4fcc
unknown <umail2bilal@gmail.com>
2025-07-13 14:01:30 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Manrope } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] });
 const manrope = Manrope({ subsets: ['latin'] });
@@ -50,6 +51,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={`${inter.className} ${manrope.className}`}>{children}</body>
     </html>
   );
